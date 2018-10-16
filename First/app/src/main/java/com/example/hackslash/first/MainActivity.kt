ba1a3c46d2bb11a391b83ff999f8589382255504
Mikheil Maisuradze <mikheil.maisuradze.1@btu.edu.ge>
2018-10-16 05:35:15 +0000

    MainActivity.kt edited online with Bitbucket

--- First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
+++ First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
@@ -9,22 +9,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editText: EditText
-
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Check.setOnClickListener {
-            editText = findViewById<View>(R.id.email) as EditText
-            val email = editText.text.toString()
-            editText = findViewById<View>(R.id.password) as EditText
-            val password = editText.text.toString()
-
-            if (email == "email@example.com" && password == "password") {
-                val intent = Intent(this, activity_galley::class.java)
-                startActivity(intent)
-            }
+                autorize()
+        }
+    }
+    fun autorize() {
+        if (email.text.toString().equels("email@example.com") && password.text.toString().equals("password")) {
+            val intent = Intent(this, activity_galley::class.java)
+            startActivity(intent)
         }
     }
 }
