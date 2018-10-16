4b3126785d004f1610db29ee8b566d44159001c0
hackslash <mikheil.maisuradze.1@btu.edu.ge>
2018-10-16 18:52:14 +0400

    Check all fields Commit

--- First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
+++ First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +14,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Check.setOnClickListener {
-            authorize()
+        signinMainActivityButton.setOnClickListener {
+            checkallfields()
+        }
+
+        registerMainActivityButton.setOnClickListener{
+            register()
         }
     }
     private fun authorize() {
-        if (email.text.toString().equals("email@example.com") && password.text.toString().equals("password")) {
-            val intent = Intent(this, activity_galley::class.java)
-            startActivity(intent)
+        val gallery = Intent(this, activity_galley::class.java)
+        startActivity(gallery)
+    }
+    private fun register(){
+        val register = Intent(this, RegisterActivity::class.java)
+        startActivity(register)
+    }
+
+    private fun checkallfields(){
+        if (emailMainActivityEditText.text.toString().isEmpty() && passwordMainActivityEditText.text.toString().isEmpty()){
+            Toast.makeText(this,"Please fill all fields", Toast.LENGTH_SHORT).show()
+        }else if(emailMainActivityEditText.text.toString().isEmpty()){
+            Toast.makeText(this,"Please enter your email", Toast.LENGTH_SHORT).show()
+        }else if(passwordMainActivityEditText.text.toString().isEmpty()){
+            Toast.makeText(this,"Please enter your password", Toast.LENGTH_SHORT).show()
+        }else if(emailMainActivityEditText.text.toString().equals("email@example.com") && passwordMainActivityEditText.text.toString().equals("password")) {
+            authorize()
+        }else{
+            Toast.makeText(this,"Email or Password is incorrect", Toast.LENGTH_SHORT).show()
         }
     }
-}
 
+}
---
+++ First/app/src/main/java/com/example/hackslash/first/Tools.kt
@@ -0,0 +1,10 @@
+package com.example.hackslash.first
+
+public class Tools {
+    companion object {
+        @JvmStatic val EMAIL_REGEX = "^[A-Za-z](.*)([@]{1})(.{1,})(.)(.{1,})";
+        fun isEmailValid(email: String): Boolean {
+            return EMAIL_REGEX.toRegex().matches(email);
+        }
+    }
+}
