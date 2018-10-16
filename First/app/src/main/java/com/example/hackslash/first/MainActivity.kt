840fed22fc5c5c824da65b03c63ccdc7dd851384
hackslash <mikheil.maisuradze.1@btu.edu.ge>
2018-10-16 21:00:27 +0400

    Email Pattern

--- First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
+++ First/app/src/main/java/com/example/hackslash/first/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.hackslash.first
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
